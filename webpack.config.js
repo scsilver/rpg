89ada9b90b81f651a4ba96eabb78b0d9150ec5e9
Scott Silverstein <scsilver.umd@gmai.com>
2018-08-15 23:06:02 -0600

    add linting and dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,33 @@
-{
-    "mode": "development",
-    "entry": "src/index.js",
-    "output": {
-        "path": __dirname+'/static',
-        "filename": "[name].[chunkhash:8].js"
-    },
-    "module": {
-        "rules": [
-            {
-                "enforce": "pre",
-                "test": /\.(js|jsx)$/,
-                "exclude": /node_modules/,
-                "use": "eslint-loader"
-            },
-            {
-                "test": /\.(js|jsx)$/,
-                "exclude": /node_modules/,
-                "use": {
-                    "loader": "babel-loader",
-                    "options": {
-                        "presets": [
-                            "env",
-                            "react"
-                        ]
-                    }
-                }
-            }
-        ]
-    }
-}
+module.exports = {
+  mode: "development",
+  entry: "src/index.js",
+  output: {
+    path: __dirname + "/static",
+    filename: "[name].[chunkhash:8].js"
+  },
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "eslint-loader"
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env", "react"]
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
+  }
+};

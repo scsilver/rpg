700fdbf97e00b705a1be822e0b6517f6a47dfcaf
Scott Silverstein <scsilver.umd@gmai.com>
2018-10-07 13:00:01 -0600

    add

--- assets/Characters/Skills.js
+++ assets/Characters/Skills.js
@@ -1,12 +1,12 @@
 class FightingSkills {
-  attack: number;
-  defense: number;
-  agility: number;
-  health: number;
+  attack = 0;
+  defense = 0;
+  agility = 0;
+  health = 0;
 }
 class ResourcingSkills {
-  mining: number;
-  chopping: number;
+  mining = 0;
+  chopping = 0;
 }
 
 const skills = {
